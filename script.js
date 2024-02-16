dcc8f8fa2a63b8e6ab1b2e53c80773bf201ad9d2
myself-nate <iosif.zainescu@yahoo.com>
2024-02-16 16:30:16 +0200

    h

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const icon = document.querySelector('.faq-icon');
+const p = document.querySelectorAll('.faq-p');
+const cards = document.querySelectorAll('.card');
+console.log(p);
+
+const openQuestion = function (e) {
+	e.preventDefault();
+	p.classList.toggle('hidden');
+};
+
+cards.forEach((card) =>
+	card.addEventListener('click', (e) => {
+		e.preventDefault();
+		p.forEach((pr) => pr.classList.toggle('hidden'));
+	})
+);
+
+window.onclick = (e) => {
+	console.log(e.target);
+};
